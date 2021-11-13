4a79603d6829c19654e0106167be50d42cab9f66
Karthikeyan461994 <karthithilip@gmail.com>
2021-11-13 18:18:05 +0530

    New Class

---
+++ src/test/java/com/hexa/GITDemo.java
@@ -0,0 +1,17 @@
+package com.hexa;
+
+public class GITDemo {
+private void one() {
+	// TODO Auto-generated method stub
+
+}
+private void two() {
+	// TODO Auto-generated method stub
+
+}
+private void three() {
+	// TODO Auto-generated method stub
+
+}
+
+}
